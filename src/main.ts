60be574c693f370427f06de358f48e6186fab5a5
core <coremotor@outlook.com>
2023-11-21 14:20:21 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { json } from 'express';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./certs/key.pem'),
-    cert: fs.readFileSync('./certs/cert.pem'),
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-  };
-
   const app = await NestFactory.create(AppModule, {
-    httpsOptions,
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
   app.enableCors({
     allowedHeaders: '*',
