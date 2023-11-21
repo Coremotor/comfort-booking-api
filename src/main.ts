fe6c3e68f5a7956318ead88a89b9a9042549c548
core <coremotor@outlook.com>
2023-11-21 12:03:33 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,9 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
   app.enableCors({
-    allowedHeaders: [
-      'Cache-Control',
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'X-Access-Token',
-      'x-api-token',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Request-Method',
-      'Access-Control-Allow-Origin',
-      'Authorization',
-      'Authorized',
-    ],
-    exposedHeaders: ['Content-Disposition'],
+    allowedHeaders: '*',
     credentials: true,
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: ['https://admin.for-test-apps.ru', '*'],
-    maxAge: 60 * 60 * 24 * 365,
-    preflightContinue: false,
+    origin: '*',
   });
   app.use(json({ limit: '50mb' }));
 
