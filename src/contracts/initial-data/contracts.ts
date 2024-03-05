cb8188071ab1eb1e8d263eab5b1f3d4cf4eb1fdc
Andrei Demakov <andrei.demakov@comfortbooking.ru>
2024-03-05 11:55:02 +0300

    dev

--- src/contracts/initial-data/contracts.ts
+++ src/contracts/initial-data/contracts.ts
@@ -11,7 +11,29 @@ export const contracts: TContract[] = [
     contractStatus: ContractStatusEnum.Active,
     years: [2023, 2022, 2021, 2020],
     type: 'paper',
-    contractConditions: [{ id: 1, status: ContractConditionStatusEnum.Active }],
+    contractConditions: [
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 1,
+        name: 'Самый лучший тариф от TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС»',
+        provider: [
+          { id: '13264', name: 'Былинкино' },
+          { id: '83264', name: 'Пылинкино' },
+          { id: '1864', name: 'ФГУП Гостиница Академическая в Москве' },
+          {
+            id: '33284',
+            name: 'ФГУП Гостиница Золотое кольцо УД Президента РФ',
+          },
+        ],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.Active,
+      },
+    ],
   },
   {
     contractId: 2,
@@ -20,7 +42,19 @@ export const contracts: TContract[] = [
     years: [2023, 2022, 2021],
     type: 'paper',
     contractConditions: [
-      { id: 1, status: ContractConditionStatusEnum.AcceptWaiting },
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 3,
+        name: 'Специальный 2,3%, с СБП 1,5%, оплата в кредит 1%, оплата частями 5,4%',
+        provider: [{ id: '13264', name: 'Былинкино' }],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.AcceptWaiting,
+      },
     ],
   },
   {
@@ -29,9 +63,7 @@ export const contracts: TContract[] = [
     contractStatus: ContractStatusEnum.Suspended,
     years: [2022, 2021, 2020, 2019],
     type: 'paper',
-    contractConditions: [
-      { id: 1, status: ContractConditionStatusEnum.Expired },
-    ],
+    contractConditions: [],
   },
   {
     contractId: 4,
@@ -40,7 +72,19 @@ export const contracts: TContract[] = [
     years: [2023],
     type: 'paper',
     contractConditions: [
-      { id: 1, status: ContractConditionStatusEnum.AcceptWaiting },
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 2,
+        name: 'Лучший тариф 4-54',
+        provider: [{ id: '13264', name: 'Былинкино' }],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.AcceptWaiting,
+      },
     ],
   },
   {
@@ -50,7 +94,19 @@ export const contracts: TContract[] = [
     years: [2023],
     type: 'paper',
     contractConditions: [
-      { id: 1, status: ContractConditionStatusEnum.ClientDeclined },
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 4,
+        name: 'Тариф',
+        provider: [{ id: '13264', name: 'Былинкино' }],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.ClientDeclined,
+      },
     ],
   },
   {
@@ -59,7 +115,29 @@ export const contracts: TContract[] = [
     contractStatus: ContractStatusEnum.Active,
     years: [],
     type: 'offer',
-    contractConditions: [{ id: 1, status: ContractConditionStatusEnum.Active }],
+    contractConditions: [
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 1,
+        name: 'Самый лучший тариф от TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС»',
+        provider: [
+          { id: '13264', name: 'Былинкино' },
+          { id: '83264', name: 'Пылинкино' },
+          { id: '1864', name: 'ФГУП Гостиница Академическая в Москве' },
+          {
+            id: '33284',
+            name: 'ФГУП Гостиница Золотое кольцо УД Президента РФ',
+          },
+        ],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.Active,
+      },
+    ],
   },
   {
     contractId: 6,
@@ -67,7 +145,21 @@ export const contracts: TContract[] = [
     contractStatus: ContractStatusEnum.AcceptWaiting,
     years: [2023],
     type: 'offer',
-    contractConditions: [{ id: 1, status: ContractConditionStatusEnum.Active }],
+    contractConditions: [
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 3,
+        name: 'Специальный 2,3%, с СБП 1,5%, оплата в кредит 1%, оплата частями 5,4%',
+        provider: [{ id: '13264', name: 'Былинкино' }],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.AcceptWaiting,
+      },
+    ],
   },
   {
     contractId: 7,
@@ -76,7 +168,19 @@ export const contracts: TContract[] = [
     years: [2023],
     type: 'offer',
     contractConditions: [
-      { id: 1, status: ContractConditionStatusEnum.ClientDeclined },
+      {
+        acceptDate: new Date().toLocaleDateString('ru-RU'),
+        startDate: new Date().toLocaleDateString('ru-RU'),
+        endDate: new Date().toLocaleDateString('ru-RU'),
+        id: 4,
+        name: 'Тариф',
+        provider: [{ id: '13264', name: 'Былинкино' }],
+        specialConditions:
+          'ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» является оператором по обработке персональных данных в соответствии с Федеральным законом «О персональных данных» от 27.07.2006 N 152-ФЗ, а также процессором по обработке персональных данных по Регламенту ЕС 2016/679 от 27 апреля 2016 г. При использовании TravelLine: Platform ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС» действует по поручению оператора, Средства/Объекта размещения, в соответствии с п. 3 ст. 6 Федерального закона РФ "О персональных данных" от 27.07.2006 N 152-ФЗ.\n' +
+          'Если у вас появились предложения или вопросы по нашей Политике конфиденциальности, пишите по электронной почте privacy@travelline.ru или по адресу Российская Федерация, 424003, Республика Марий Эл, город Йошкар-Ола, Ленинский проспект 56А, ООО «ТРЭВЕЛ ЛАЙН СИСТЕМС».\n' +
+          'Подробнее: https://www.travelline.ru/contacts/',
+        status: ContractConditionStatusEnum.ClientDeclined,
+      },
     ],
   },
   {
@@ -85,13 +189,6 @@ export const contracts: TContract[] = [
     contractStatus: ContractStatusEnum.Terminated,
     years: [2023],
     type: 'offer',
-    contractConditions: [
-      // { id: 1, status: ContractConditionStatusEnum.AcceptWaiting },
-      { id: 2, status: ContractConditionStatusEnum.Active },
-      { id: 3, status: ContractConditionStatusEnum.ClientDeclined },
-      { id: 4, status: ContractConditionStatusEnum.AcceptingExpired },
-      { id: 5, status: ContractConditionStatusEnum.BeginWaiting },
-      { id: 6, status: ContractConditionStatusEnum.Expired },
-    ],
+    contractConditions: [],
   },
 ];
--- src/contracts/types/enums.ts
+++ src/contracts/types/enums.ts
@@ -1,20 +1,20 @@
 export enum ContractStatusEnum {
-  AcceptWaiting, // Ожидает акцепта
-  ContractDataConfirmWaiting, // Ожидает подтверждения данных
-  Stay, // Принят
-  Active, // Активен
-  AcceptDeclined, // Акцепт договора отклонен
-  Suspended, // Договор приостановлен
-  Terminated, // Договор расторгнут
+  AcceptDeclined = 'AcceptDeclined', // Акцепт договора отклонен
+  AcceptWaiting = 'AcceptWaiting', // Ожидает акцепта
+  Active = 'Active', // Активен
+  ContractDataConfirmWaiting = 'ContractDataConfirmWaiting', // Ожидает подтверждения данных
+  Stay = 'Stay', // Принят
+  Suspended = 'Suspended', // Договор приостановлен
+  Terminated = 'Terminated', // Договор расторгнут
 }
 
 export enum ContractConditionStatusEnum {
-  AcceptWaiting, // Ожидают акцепта
-  Active, // Активны
-  ClientDeclined, // Отклонены клиентом
-  AcceptingExpired, // Истек срок принятия условий
-  BeginWaiting, // Ожидают начала действия
-  Expired, // Завершен срок действия
+  AcceptingExpired = 'AcceptingExpired', // Истек срок принятия условий
+  AcceptWaiting = 'AcceptWaiting', // Ожидают акцепта
+  Active = 'Active', // Активны
+  BeginWaiting = 'BeginWaiting', // Ожидают начала действия
+  ClientDeclined = 'ClientDeclined', // Отклонены клиентом
+  Expired = 'Expired', // Завершен срок действия
 }
 
 export type TContract = {
@@ -23,5 +23,16 @@ export type TContract = {
   contractStatus: ContractStatusEnum;
   years: number[];
   type: 'paper' | 'offer';
-  contractConditions: { id: number; status: ContractConditionStatusEnum }[];
+  contractConditions: TCondition[];
+};
+
+export type TCondition = {
+  acceptDate: string;
+  startDate: string;
+  endDate: string;
+  id: number;
+  name: string;
+  provider: { id: string; name: string }[];
+  specialConditions: string;
+  status: ContractConditionStatusEnum;
 };
